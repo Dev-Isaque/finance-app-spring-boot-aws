4725b76702ed4f77e233aa98f8bff1a2e4f42ae8
Dev-Isaque <isaquesbl123@gmail.com>
2025-02-11 10:04:35 -0300

    Normalizando finais de linha

---
+++ src/main/java/com/isaquesoares/financeapp/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.isaquesoares.financeapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.isaquesoares.financeapp.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    @Query("{ 'email' : { $regex: ?0, $options: 'i' } }") 
+    Optional<User> findByEmail(String email);
+}
